9e82360798361d05d2986ec95f32aaff4a0ff1c2
Syahfril Nizammudin <75606963+Syahfril@users.noreply.github.com>
2021-12-23 08:57:25 +0700

    Add files via upload

---
+++ real time face detection/codeRT.py
@@ -0,0 +1,62 @@
+import face_recognition
+import cv2
+import numpy as np 
+
+video_capture = cv2.VideoCapture(0)
+
+me_image = face_recognition.load_image_file("me.jpg")
+me_face_encoding = face_recognition.face_encodings(me_image)[0]
+
+emma_image = face_recognition.load_image_file("emma.jpg")
+emma_face_encoding = face_recognition.face_encodings(emma_image)[0]
+
+known_face_encodings = [me_face_encoding, emma_face_encoding]
+
+known_face_names = ["syah", "emma"]
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    ret, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    if process_this_frame:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "unknow"
+
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+            
+            face_names.append(name)
+    process_this_frame = not process_this_frame
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        cv2.rectangle(frame, (left, top), (right,bottom), (0, 255, 0), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0,(255,255,255), 1)
+    
+    cv2.imshow('gummycam',frame)
+    g = cv2.waitKey(30) & 0xff
+    if g == 27:
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+        
